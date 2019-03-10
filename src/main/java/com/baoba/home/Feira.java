296bcf3a48ae1baa1fa6eb54ad7363e40b08b0be
Paulo Cruz <paulocac_1@hotmail.com>
2019-03-10 16:03:55 -0300

    Inclusao servico Feira

---
+++ src/main/java/com/baoba/home/Feira.java
@@ -0,0 +1,53 @@
+package com.baoba.home;
+
+public class Feira {
+	
+	private String codigoBarras;
+	private String nomeProduto;
+	private String preco;
+	private int quantidade;
+	private String dataVencimento;
+	
+	public Feira(String codigoBarras, String nomeProduto, String preco, int quantidade, String dataVencimento) {
+		super();
+		this.codigoBarras = codigoBarras;
+		this.nomeProduto = nomeProduto;
+		this.preco = preco;
+		this.quantidade = quantidade;
+		this.dataVencimento = dataVencimento;
+	}
+		
+	public String getCodigoBarras() {
+		return codigoBarras;
+	}
+	public void setCodigoBarras(String codigoBarras) {
+		this.codigoBarras = codigoBarras;
+	}
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+	public String getPreco() {
+		return preco;
+	}
+	public void setPreco(String preco) {
+		this.preco = preco;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	public String getDataVencimento() {
+		return dataVencimento;
+	}
+	public void setDataVencimento(String dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
+	
+	
+
+}
---
+++ src/main/java/com/baoba/home/FeiraResource.java
@@ -0,0 +1,72 @@
+package com.baoba.home;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.google.gson.Gson;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+@RestController
+@RequestMapping(value = "/feiras")
+public class FeiraResource {
+	
+	public static final String URL = "https://baoba-home.firebaseio.com/Feira.json?auth=RLiDBgOkvvXml0sIEVhYBMCI5FgUkNzeLeAeNSsY";
+
+	JSONObject dadosFeiraOriginalJson;
+	JSONArray listaFeiraNomes;
+	List<Feira> listaFeira;
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String obterProdutosFeira() {
+		
+		listaFeira = new ArrayList<Feira>();
+		listaFeiraNomes = new JSONArray();
+		
+		try {
+			
+			OkHttpClient client = new OkHttpClient();
+			Request request = new Request.Builder().url(URL).build();
+			Response response = client.newCall(request).execute();
+			
+			dadosFeiraOriginalJson = new JSONObject(response.body().string());
+			
+			listaFeiraNomes = dadosFeiraOriginalJson.names();
+			
+			for(int i=0; i < listaFeiraNomes.length(); i++)
+			{
+				String nome = listaFeiraNomes.get(i).toString();
+				
+				JSONObject dadosUnit = dadosFeiraOriginalJson.getJSONObject(nome);
+				
+				Feira feira = new Feira(
+						dadosUnit.getString("codigoBarras"), 						
+						dadosUnit.getString("nomeProduto"), 
+						dadosUnit.getString("preco"), 
+						dadosUnit.getInt("quantidade"), 
+						dadosUnit.getString("dataVencimento"));
+				
+				listaFeira.add(feira);
+			}
+			
+			String feiraConvertida = new Gson().toJson(listaFeira).toString();
+			
+			return feiraConvertida;
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+		return "Rest Feira esta funcionando";
+	}
+
+}
